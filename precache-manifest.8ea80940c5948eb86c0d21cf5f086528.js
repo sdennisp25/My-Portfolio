7e14456659236bf6b92c889b13cf46f5ab179035
Dennis The Menace <sdennisp25@hotmail.com>
2019-03-22 23:35:19 -0600

    Updates

---
+++ precache-manifest.8ea80940c5948eb86c0d21cf5f086528.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "43abf94b8e1686184f89d4073bb9eda6",
+    "url": "/My-Portfolio/static/media/Trivia Survey.43abf94b.png"
+  },
+  {
+    "revision": "ff9ad12bf3672997bd12",
+    "url": "/My-Portfolio/static/css/main.9f37d053.chunk.css"
+  },
+  {
+    "revision": "b7d4bf292e75006047a4",
+    "url": "/My-Portfolio/static/js/runtime~main.b7d4bf29.js"
+  },
+  {
+    "revision": "38e9280959dd5f0d5c78",
+    "url": "/My-Portfolio/static/js/2.38e92809.chunk.js"
+  },
+  {
+    "revision": "162d7ee05fb80aec4c255455426e1802",
+    "url": "/My-Portfolio/static/media/MyClassPhoto.162d7ee0.jpg"
+  },
+  {
+    "revision": "c2ea9a5a1e63441bcfdf43bd17380a32",
+    "url": "/My-Portfolio/static/media/friendfinder.c2ea9a5a.png"
+  },
+  {
+    "revision": "4ba9d231141e7e8adf0bf9a87def7c16",
+    "url": "/My-Portfolio/static/media/eatdaburger.4ba9d231.png"
+  },
+  {
+    "revision": "043d4a53e820937ebcf8fac219bc8788",
+    "url": "/My-Portfolio/static/media/Train Timesheet.043d4a53.png"
+  },
+  {
+    "revision": "ff9ad12bf3672997bd12",
+    "url": "/My-Portfolio/static/js/main.ff9ad12b.chunk.js"
+  },
+  {
+    "revision": "82bece9b58c77280129a9bf35eac80a1",
+    "url": "/My-Portfolio/static/media/Banksys.82bece9b.png"
+  },
+  {
+    "revision": "985910c809f4e7d15d1e3d66a659cf71",
+    "url": "/My-Portfolio/static/media/alleyart.985910c8.png"
+  },
+  {
+    "revision": "95b5d318df5362d536c87b88330a924f",
+    "url": "/My-Portfolio/static/media/fixedsigns.95b5d318.png"
+  },
+  {
+    "revision": "071567c7d38f5ee93d1378c814687855",
+    "url": "/My-Portfolio/static/media/Psychic Game.071567c7.png"
+  },
+  {
+    "revision": "e0dc1847e07487b923a1692dde109081",
+    "url": "/My-Portfolio/static/media/SWhangman.e0dc1847.png"
+  },
+  {
+    "revision": "ebc2956f4cc4028ba6888bb133caa899",
+    "url": "/My-Portfolio/static/media/SWgame.ebc2956f.png"
+  },
+  {
+    "revision": "050437ce4ad9e8e0aa2c28893a4e97fa",
+    "url": "/My-Portfolio/static/media/Giphy API.050437ce.png"
+  },
+  {
+    "revision": "9de19c059681d4098b1a0b76cf6fc2bd",
+    "url": "/My-Portfolio/index.html"
+  }
+];
